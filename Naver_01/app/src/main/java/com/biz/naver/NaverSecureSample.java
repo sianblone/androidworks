ec4892d53e6103a66ad7cef96630839e9501c383
sianblone <sianblone@gmail.com>
2020-02-24 09:21:38 +0900

    1

---
+++ Naver_01/app/src/main/java/com/biz/naver/NaverSecureSample.java
@@ -0,0 +1,8 @@
+package com.biz.naver;
+
+public class NaverSecureSample {
+
+    public static final String NAVER_ID = "INPUT CLIENT_ID";
+    public static final String NAVER_SEC = "INPUT CLIENT_SEC";
+
+}
